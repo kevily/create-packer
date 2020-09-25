2eed0052965e2ca9600b542f0aba923a5f2173d2
1k <bug_zero@163.com>
2020-09-25 13:01:22 +0800

    add mkdir selector

--- bin/index.js
+++ bin/index.js
@@ -4,24 +4,44 @@ const path = require('path')
 const fsExtra = require('fs-extra')
 const fs = require('fs')
 const inquirer = require('inquirer')
+const chalk = require('chalk')
 
 const templateSrc = path.join(__dirname, '../template')
-const output = process.cwd()
-const choices = fs.readdirSync(templateSrc)
+let output = process.cwd()
 const entry = new Map()
-
-choices.forEach((dirname) => {
+const choices = fs.readdirSync(templateSrc).filter(dirname => {
     if (dirname.startsWith('.')) {
         return false
     }
     entry.set(dirname, path.join(templateSrc, dirname))
+    return true
 })
 
 inquirer
-    .prompt([{ type: 'list', name: 'template', choices }])
-    .then(({ template }) => {
+    .prompt([
+        { type: 'list', name: 'template', choices },
+        {
+            type: 'input',
+            name: 'mkdir',
+            message: 'mkdir(y/n):'
+        },
+        {
+            type: 'input',
+            name: 'dirname',
+            message: 'input dirname(template):',
+            when({ mkdir }) {
+                return mkdir.toLowerCase() === 'y'
+            }
+        }
+    ])
+    .then(({ template, mkdir, dirname }) => {
+        if (mkdir) {
+            output = path.join(output, dirname || 'template')
+            fsExtra.mkdirSync(output)
+        }
         fsExtra.copySync(entry.get(template), output)
+        console.log(chalk.yellow('created（*＾3＾）'))
     })
-    .catch((error) => {
-        console.log('error', error)
+    .catch(error => {
+        throw new Error(error)
     })
