0477f751c5d01cbc66788e57ba55a6e568f2b0fa
kevily <bug_zero@163.com>
2021-02-26 20:34:47 +0800

    ..

--- template/webpack/.stylelintrc.js
+++ template/webpack/.stylelintrc.js
@@ -1,6 +1,21 @@
 module.exports = {
     extends: 'stylelint-config-standard',
     rules: {
-        indentation: 4
+        indentation: 4,
+        'comment-empty-line-before': 'never',
+        'no-empty-source': null,
+        'no-descending-specificity': null,
+        'at-rule-no-unknown': [
+            true,
+            {
+                ignoreAtRules: ['function', 'if', 'each', 'include', 'mixin', 'extend']
+            }
+        ],
+        'selector-pseudo-class-no-unknown': [
+            true,
+            {
+                ignorePseudoClasses: ['global']
+            }
+        ]
     }
 }
