4c2109f3b2825f6166fdde1d0c971dea0f397cdc
kevily <bug_zero@163.com>
2021-07-30 22:01:39 +0800

    Docs: add cz config, webpack: add dev-hot

--- template/webpack/webpack_config/config.js
+++ template/webpack/webpack_config/config.js
@@ -10,24 +10,5 @@ module.exports = {
     OUTPUT,
     STATIC_DIR: 'static',
     gzip: false,
-    devServer: {
-        host: 'localhost',
-        port: 8080,
-        inline: true,
-        hot: true,
-        publicPath: 'http://localhost:8080',
-        open: true,
-        historyApiFallback: true,
-        proxy: {
-            '/api': {
-                target: 'http://127.0.0.1:3000',
-                changeOrigin: true,
-                secure: false,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            }
-        }
-    },
     scopeClassName: '[name]__[local]--[hash:base64:5]'
 }
--- template/webpack/webpack_config/genBaseConfig.js
+++ template/webpack/webpack_config/genBaseConfig.js
@@ -16,6 +16,25 @@ module.exports = function genBaseConfig(env) {
         },
         bail: true,
         mode: env.prod ? 'production' : 'development',
+        devServer: {
+            host: 'localhost',
+            port: 8080,
+            inline: true,
+            hot: true,
+            publicPath: 'http://localhost:8080',
+            open: true,
+            historyApiFallback: true,
+            proxy: {
+                '/api': {
+                    target: 'http://127.0.0.1:3000',
+                    changeOrigin: true,
+                    secure: false,
+                    pathRewrite: {
+                        '^/api': ''
+                    }
+                }
+            }
+        },
         module: {
             rules: [
                 {
