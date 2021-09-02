cde2ffcb1b63dc4c8644da56b7c3ddf507332a99
kevily <bug_zero@163.com>
2021-09-02 21:02:04 +0800

    Update: update bin/index

--- bin/index.js
+++ bin/index.js
@@ -4,15 +4,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Create_1 = require("./clis/Create");
 const commander_1 = require("commander");
 const create = new Create_1.Create();
-let dirname = '';
-commander_1.program
-    .argument('[dirname]')
-    .action(name => {
-    dirname = name || '';
-})
-    .option('-c', 'Create project.')
-    .parse(process.argv);
+commander_1.program.option('-c [dirname]', 'Create project.').parse(process.argv);
 const options = commander_1.program.opts();
 if (options.c) {
+    let dirname = '';
+    if (options.c !== true) {
+        dirname = options.c;
+    }
     create.onCreate(dirname);
 }
--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,14 @@ import { Create } from './clis/Create'
 import { program } from 'commander'
 
 const create = new Create()
-let dirname = ''
-program
-    .argument('[dirname]')
-    .action(name => {
-        dirname = name || ''
-    })
-    .option('-c', 'Create project.')
-    .parse(process.argv)
+program.option('-c [dirname]', 'Create project.').parse(process.argv)
 
 const options = program.opts()
 
 if (options.c) {
+    let dirname = ''
+    if (options.c !== true) {
+        dirname = options.c
+    }
     create.onCreate(dirname)
 }
