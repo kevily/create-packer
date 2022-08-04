03c76f72ccf51b366a47e27d8d460f6fa83e6453
1k <bug_zero@163.com>
2022-08-04 12:48:57 +0800

    Update: nest, react-cra

--- template/nest/src/utils/transform.interceptor.ts
+++ template/nest/src/utils/transform.interceptor.ts
@@ -1,4 +1,9 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common'
+import {
+    Injectable,
+    NestInterceptor,
+    ExecutionContext,
+    CallHandler,
+} from '@nestjs/common'
 import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 
@@ -7,8 +12,15 @@ export interface Response<T> {
 }
 
 @Injectable()
-export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
-    intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
-        return next.handle().pipe(map(data => ({ code: 200, message: '', data })))
+export class TransformInterceptor<T>
+    implements NestInterceptor<T, Response<T>>
+{
+    intercept(
+        context: ExecutionContext,
+        next: CallHandler,
+    ): Observable<Response<T>> {
+        return next
+            .handle()
+            .pipe(map((data) => ({ code: 200, message: '', data })))
     }
 }
