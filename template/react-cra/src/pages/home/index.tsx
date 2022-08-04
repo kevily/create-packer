3bbc72a0eb74886e93726d6cdaf5449b121f3029
1k <bug_zero@163.com>
2022-08-04 13:00:58 +0800

    Fix:

--- template/react-cra/src/pages/home/index.tsx
+++ template/react-cra/src/pages/home/index.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import homeStore from './home.store'
-import { useRecoilValue } from 'recoil'
 import './Home.scss'
 
 export interface HomePropsType {}
 
 const App: React.FC<HomePropsType> = () => {
-    const store = useRecoilValue(homeStore)
-
-    console.log('home:store', store)
     return <div className="home">home</div>
 }
 
