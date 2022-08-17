c6092622cc4f1d5855b194c538f934a39795aeac
1k <bug_zero@163.com>
2022-08-17 22:43:10 +0800

    .

--- template/vue-vite/tailwind.config.cjs
+++ template/vue-vite/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./index.html', './src/**/*.{ts,tsx}'],
+    content: ['./index.html', './src/**/*.{vue,ts,tsx}'],
     theme: {
         extend: {}
     },
