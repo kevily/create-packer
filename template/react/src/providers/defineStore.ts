a38cf3cd0f97b6a9cdca80ef34ba1795593f8c01
1k <bug_zero@163.com>
2022-09-14 22:46:10 +0800

    Fix:

--- template/react/src/providers/defineStore.ts
+++ template/react/src/providers/defineStore.ts
@@ -1,28 +1,34 @@
-import { StateCreator } from 'zustand/vanilla'
 import create from 'zustand'
 import { combine } from 'zustand/middleware'
 
+type setStateType<S> = (updater: (state: S) => void) => void
+type actionsType<S, A> = {
+    reset: () => void
+    setState: setStateType<S>
+} & A
 export interface defineStoreOptionsType<S extends object, A extends object> {
     state: () => S
-    actions?: StateCreator<{ state: S }, [], [], A>
+    actions?: (setState: setStateType<S>, getState: () => S) => A
 }
-export function defineStore<S extends object, U extends object>(
-    options: defineStoreOptionsType<S, U>
+export function defineStore<S extends object, A extends object>(
+    options: defineStoreOptionsType<S, A>
 ) {
     return create(
-        combine({ state: options.state() }, (setState, getState, store, $$storeMutations) => {
+        combine({ state: options.state() }, (setState, getState) => {
+            const _setState: setStateType<S> = updater => {
+                setState(store => {
+                    updater(store.state)
+                    return { state: store.state }
+                })
+            }
+            const _getState = () => getState().state
             return {
                 reset: () => {
                     setState({ state: options.state() })
                 },
-                updater: (updater: (state: S) => void) => {
-                    setState(store => {
-                        updater(store.state)
-                        return { state: store.state }
-                    })
-                },
-                ...(options.actions?.(setState, getState, store, $$storeMutations) || {})
-            }
+                setState: _setState,
+                ...(options.actions?.(_setState, _getState) || {})
+            } as actionsType<S, A>
         })
     )
 }
