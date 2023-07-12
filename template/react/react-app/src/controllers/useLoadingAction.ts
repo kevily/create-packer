8fb25f9a5f9af6dfede1772f26423c109184e443
1k <bug_zero@163.com>
2023-07-12 22:36:23 +0800

    feat: react-app:  useLoadingAction add debounce

--- template/react/react-app/src/controllers/useLoadingAction.ts
+++ template/react/react-app/src/controllers/useLoadingAction.ts
@@ -1,18 +1,27 @@
-import { useCallback, useState, DependencyList } from 'react'
+import { useCallback, useState, DependencyList, useRef } from 'react'
 
 export default function useLoadingAction<C extends (...arg: any) => Promise<any> | any>(
     callback: C,
-    deps: DependencyList
-): [boolean, C] {
+    deps: DependencyList,
+    debounce = true
+) {
     const [loading, setLoading] = useState(false)
+    const loadingSync = useRef(loading)
+    function $setLoading(loading: boolean) {
+        setLoading(loading)
+        loadingSync.current = loading
+    }
     const action = useCallback(async (...arg: any) => {
+        if (debounce && loadingSync.current) {
+            return
+        }
         try {
-            setLoading(true)
+            $setLoading(true)
             return await callback(...arg)
         } finally {
-            setLoading(false)
+            $setLoading(false)
         }
-    }, deps)
+    }, deps) as C
 
-    return [loading, action as C]
+    return [loading, action] as const
 }
