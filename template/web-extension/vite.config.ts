3c6d5f892cd9a062f73ba605489093ca1dc1ef08
1k <bug_zero@163.com>
2023-07-25 23:39:36 +0800

    fix: web-extension

--- template/web-extension/vite.config.ts
+++ template/web-extension/vite.config.ts
@@ -1,4 +1,3 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
@@ -30,7 +29,7 @@ export default defineConfig(({ mode }) => {
         ],
         resolve: {
             alias: {
-                '@': path.join(__dirname, 'src')
+                '@': __dirname
             }
         },
         esbuild: {
