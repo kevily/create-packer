af046bdf9a7373d081c22a3e8e71060c5a98e311
1k <bug_zero@163.com>
2023-09-19 11:30:16 +0800

    chore: vue-app, react-app

--- template/web-app/react/scripts/createChunks.ts
+++ template/web-app/react/scripts/createChunks.ts
@@ -1,10 +1,9 @@
-import { concat, forEach, isArray, isRegExp, keys, remove, size } from 'lodash-es'
+import { concat, forEach, includes, isArray, isRegExp, keys, remove, size } from 'lodash-es'
 import pkg from '../package.json'
 
 export function createChunks(chunks: { [key: string]: Array<string | RegExp> }) {
-    const result: { [key: string]: string[] } = {
-        vendor: keys(pkg.dependencies)
-    }
+    const vendor = keys(pkg.dependencies)
+    const result: { [key: string]: string[] } = {}
 
     forEach(chunks, (values, key) => {
         if (!isArray(result[key])) {
@@ -13,14 +12,15 @@ export function createChunks(chunks: { [key: string]: Array<string | RegExp> }) 
         forEach(values, value => {
             let modules: string[] = []
             if (isRegExp(value)) {
-                modules = remove(result.vendor, name => value.test(name))
+                modules = remove(vendor, name => value.test(name))
             } else {
-                modules = remove(result.vendor, name => name === value)
+                modules = remove(vendor, name => name === value)
             }
             if (size(modules) > 0) {
                 result[key] = concat(result[key], modules)
             }
         })
     })
+    result.vendor = vendor
     return result
 }
