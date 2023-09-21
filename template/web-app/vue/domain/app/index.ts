844abcdb29ef78aaf9effc85825a057dee5bf756
1k <bug_zero@163.com>
2023-09-21 15:02:07 +0800

    feat: s

--- template/web-app/vue/domain/app/index.ts
+++ template/web-app/vue/domain/app/index.ts
@@ -1,4 +1,2 @@
 export { default as app } from './app'
-export { default as AppLayout } from './app-layout.vue'
-export { default as AppNotFound } from './app-not-found.vue'
 export * from './createComponentInstance'
