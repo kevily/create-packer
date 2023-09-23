b546ce891447e19bd6a9df66a33f4007c0b8f4c6
1k <bug_zero@163.com>
2023-09-23 14:22:58 +0800

    chore: svelte-app

--- template/web-app/svelte/vite.config.ts
+++ template/web-app/svelte/vite.config.ts
@@ -1,6 +1,41 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite'
+import { sveltekit } from '@sveltejs/kit/vite'
+import { includes } from 'lodash-es'
+import { createChunks } from './scripts'
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        base: env.VITE_BASE_URL,
+        plugins: [sveltekit()],
+        resolve: {
+            alias: {
+                '@': __dirname
+            }
+        },
+        esbuild: {
+            drop: includes(['production', 'analyse'], mode) ? ['console', 'debugger'] : []
+        },
+        build: {
+            sourcemap: mode === 'analyse',
+            reportCompressedSize: mode === 'analyse',
+            rollupOptions: {
+                output: {
+                    manualChunks: createChunks({
+                        sveltejs: ['sveltejs']
+                    })
+                }
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            proxy: {
+                '/dev/api': {
+                    target: 'http://127.0.0.1',
+                    changeOrigin: true,
+                    rewrite: path => path.replace(/^\/dev\/api/, '')
+                }
+            }
+        }
+    }
+})
