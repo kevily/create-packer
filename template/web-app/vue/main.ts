7f0740a8b487f35393e887ebbcba3a3e446dfd30
1k <bug_zero@163.com>
2023-09-23 11:04:35 +0800

    chore: web-app

--- template/web-app/vue/main.ts
+++ template/web-app/vue/main.ts
@@ -1,6 +1,6 @@
 import { createPinia } from 'pinia'
-import router from '@/router'
-import app from './providers/app'
+import { router } from '@/router'
+import { app } from '@/domain/app'
 import './style.css'
 
 app.use(createPinia()).use(router).mount('#app')
