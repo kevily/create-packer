2cb1f6a85014d0e920670274417e20be8fde1381
1k <bug_zero@163.com>
2024-09-25 16:54:31 +0800

    feat: s

--- template/web-app/react-vite/vite-env.d.ts
+++ template/web-app/react-vite/vite-env.d.ts
@@ -3,14 +3,16 @@
 import 'styled-components'
 import { themeType } from './shared/theme'
 
-interface ImportMetaEnv {
-    readonly VITE_BASE_URL: string
-    readonly VITE_API_HOST: string
-    // 更多环境变量...
-}
+declare global {
+    interface ImportMetaEnv {
+        readonly VITE_BASE_URL: string
+        readonly VITE_API_HOST: string
+        // 更多环境变量...
+    }
 
-interface ImportMeta {
-    readonly env: ImportMetaEnv
+    interface ImportMeta {
+        readonly env: ImportMetaEnv
+    }
 }
 
 declare module 'styled-components' {
--- template/web-extension/vite-env.d.ts
+++ template/web-extension/vite-env.d.ts
@@ -2,14 +2,16 @@
 import 'styled-components'
 import { themeType } from './shared/styles'
 
-interface ImportMetaEnv {
-    readonly VITE_API_HOST: string
-    readonly VITE_APP_ID: string
-    // 更多环境变量...
-}
+declare global {
+    interface ImportMetaEnv {
+        readonly VITE_API_HOST: string
+        readonly VITE_APP_ID: string
+        // 更多环境变量...
+    }
 
-interface ImportMeta {
-    readonly env: ImportMetaEnv
+    interface ImportMeta {
+        readonly env: ImportMetaEnv
+    }
 }
 
 declare module 'styled-components' {
