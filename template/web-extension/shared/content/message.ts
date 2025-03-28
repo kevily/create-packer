00d7d438034f77121357cf4764416acbe7e7ff89
1k <bug_zero@163.com>
2025-03-28 09:21:10 +0800

    chore: update web-extension

--- template/web-extension/shared/content/message.ts
+++ template/web-extension/shared/content/message.ts
@@ -6,17 +6,17 @@ export type messageType = {
 }
 
 export const { sendMessage, onMessage } = defineExtensionMessaging<messageType>()
-
 export async function sendToAllContent<A extends keyof messageType>(
     action: A,
     data?: Parameters<messageType[A]>[0]
 ) {
     const tabs = await chrome.tabs.query({ url: CONTENT_MATCHES })
-    return await Promise.all(
+    const result = await Promise.all(
         tabs.map(tab => {
             return sendMessage(action, data as never, { tabId: tab.id! })
         })
     )
+    return result
 }
 
 export async function sendToCurrentContent<A extends keyof messageType>(
@@ -24,5 +24,6 @@ export async function sendToCurrentContent<A extends keyof messageType>(
     data?: Parameters<messageType[A]>[0]
 ) {
     const tabs = await chrome.tabs.query({ active: true, url: CONTENT_MATCHES })
-    return await sendMessage(action, data as never, { tabId: tabs[0].id! })
+    const result = await sendMessage(action, data as never, { tabId: tabs[0].id! })
+    return result
 }
