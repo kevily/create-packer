c53ae16614f4be051f6322a10b2f3e8925ab5298
1k <bug_zero@163.com>
2026-07-30 16:59:19 +0800

    fix: vue

--- template/web-app/vue-rsbuild/src/main.ts
+++ template/web-app/vue-rsbuild/src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { router } from '@/router'
 import { App } from '@/components'
-import './styles/theme.css'
+import './main.css'
 
 createApp(App).use(createPinia()).use(router).mount('#app')
